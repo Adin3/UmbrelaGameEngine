356c70d9374c841746084dbb1164b797434ff66f
Adin3 <75613031+Adin3@users.noreply.github.com>
2022-01-14 19:31:12 +0200

    Revert "commit"
    This reverts commit 710937b2fb7bd2a1e9bebc028b56ce3199e5283d.

--- UmbrelaEngine2D/gfx/graphics.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "indexBuffer.h"
-#include "vertexArray.h"
-#include "vertexBuffer.h"
-#include "shader/shader.h"
-#include "Renderer.h"
-#include "../vendor/stb_image.h"
-#include "TextureManager.h"
